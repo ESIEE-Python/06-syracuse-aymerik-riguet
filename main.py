c6524c40b4106d0616f080d3d476c1c1b4504b47
aymerik-riguet <aymerik.riguet@edu.esiee.fr>
2024-11-28 10:40:11 +0000

    exo6

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+"""La fonction suivante permet de realiser la suite de syracuse sur des listes"""
 #### Fonctions secondaires
 
 
@@ -6,7 +7,8 @@ from plotly.graph_objects import Scatter, Figure
 
 ### NE PAS MODIFIER ###
 def syr_plot(lsyr):
-    title = "Syracuse" + " (n = " + str(lsyr[0]) + " )"
+    """fonction qui permet d'afficher un graphe de l'évolution de la liste en parametre"""
+    title = "Syracuse" + " (n = " + str(lsyr[0]) + ")"
     fig = Figure({  'layout':   { 'title': {'text': title},
                                 'xaxis': {'title': {'text':"x"}},
                                 'yaxis': {'title': {'text':"y"}},
@@ -14,12 +16,11 @@ def syr_plot(lsyr):
                 }
     )
 
-    x = [ i for i in range(len(lsyr)) ]
+    x = [ list(range(len(lsyr))) ]
     t = Scatter(x=x, y=lsyr, mode="lines+markers", marker_color = "blue")
     fig.add_trace(t)
     fig.show()
     # fig.write_html('fig.html', include_plotlyjs='cdn')
-    return None
 #######################
 
 def syracuse_l(n):
@@ -31,9 +32,15 @@ def syracuse_l(n):
     Returns:
         list: la suite de Syracuse de source n
     """
+    # votre code ici
 
-    # votre code ici 
-    l = [ ]
+    l = [n]
+    while n !=1 :
+        if n%2 == 0 :
+            n = n//2
+        else :
+            n = n*3 +1
+        l.append(n)
     return l
 
 def temps_de_vol(l):
@@ -45,11 +52,12 @@ def temps_de_vol(l):
     Returns:
         int: le temps de vol
     """
-    
     # votre code ici
-
-    n = 0
-    return n
+    tv = 0
+    for elt in l:
+        if elt == 1:
+            tv = l.index(elt) + 1
+    return tv
 
 def temps_de_vol_en_altitude(l):
     """Retourne le temps de vol en altitude d'une suite de Syracuse
@@ -62,9 +70,14 @@ def temps_de_vol_en_altitude(l):
     """
 
     # votre code ici
-
-    n = 0
-    return n
+    l0 = l[0]
+    tva = 0
+    access = True
+    for elt in l:
+        if l0 > elt and access is True:
+            tva = l.index(elt)
+            access = False
+    return tva
 
 
 def altitude_maximale(l):
@@ -76,25 +89,21 @@ def altitude_maximale(l):
     Returns:
         int: l'altitude maximale
     """
-    
     # votre code ici
-    
-    n = 0
-    return n
+    return max(l)
 
 
 #### Fonction principale
 
 
 def main():
-
+    """Fonction qui test les différentes fonction crées"""
     # vos appels à la fonction secondaire ici
     lsyr = syracuse_l(15)
-    syr_plot(lsyr)
+    #syr_plot(lsyr)
     print(temps_de_vol(lsyr))
     print(temps_de_vol_en_altitude(lsyr))
     print(altitude_maximale(lsyr))
-
 
 if __name__ == "__main__":
     main()
